fe77a37a707893dd01e976123abdb8f5143ba1f9
Coffeteer <104941061+Coffeteer@users.noreply.github.com>
2023-11-15 17:20:58 -0500

    Shiny_App_Start
    Initial framework of Shiny App.

--- Table2Cell.m
+++ Table2Cell.m
@@ -1,12 +1,8 @@
-% Load the .mat file (replace 'your_file.mat' with the actual file path)
 loaded_data = load('Cr20Mat.mat');
-
-% Extract the table from the loaded data
-data_table = loaded_data.Cr20.data;  % Replace 'your_table' with the actual variable name
+data_table = loaded_data.Cr20.data;  
 overview_table = loaded_data.Cr20.Overview;
-% Convert the table to a cell array
 data_cell = table2cell(data_table);
 overview_cell = table2cell(overview_table);
 
-filename = "Cr20Mat2.mat";
+filename = "Cr20Mat3.mat";
 save(filename)
